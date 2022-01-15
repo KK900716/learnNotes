e48a3c594f4049c113d234859a2362017a054b3d
HPME <443808626@qq.com>
2022-01-16 00:55:22 +0800

    1.16

--- MySQL/10.存储过程/存储过程.sql
+++ MySQL/10.存储过程/存储过程.sql
@@ -1,22 +1,37 @@
 存储过程
     存储过程是sql语句和流程控制语句的预编译的集合，并以一个名称存储并作为一个单元进行处理
     执行效率比select语句要高
-    a.语法结构
-        （1）CREATE PROCEDURE proc_name([proc_parameter]) [characteristics...] routine_body
-        （2）proc_parameter指定存储过程的参数列表，形式如下
-            [IN | OUT | INOUT] param_name type
-            默认为in输入参数
-        （3）characteristics存储特性同定义函数相同
-    b.过程体
-        过程体由合法的sql语句构成
-        过程体如果为复合结构则使用BEGIN...END语句
-        复合结构可以包含声明，流程控制语句
-    c.例
-        创建一个查询图书的编号、书名、价格和库存的存储过程
+    1.存储过程
+        a.语法结构
+            （1）CREATE PROCEDURE proc_name([proc_parameter]) [characteristics...] routine_body
+            （2）proc_parameter指定存储过程的参数列表，形式如下
+                [IN | OUT | INOUT] param_name type
+                默认为in输入参数
+            （3）characteristics存储特性同定义函数相同
+        b.过程体
+            过程体由合法的sql语句构成
+            过程体如果为复合结构则使用BEGIN...END语句
+            复合结构可以包含声明，流程控制语句
+        c.例
+            创建一个查询图书的编号、书名、价格和库存的存储过程
 
-        create procedure selectproc()
-        select 'abc' t;
+            create procedure selectproc()
+            select 'abc' t;
 
-        call selectproc();
+            call selectproc();
 
-        drop procedure selectproc;
+            drop procedure selectproc;
+
+            delimiter //
+            create procedure test()
+            begin
+            select * from bookcategory;
+            select * from bookcategory2;
+            end //
+            delimiter ;
+            call test();
+            drop procedure test;
+            
+            OUT的接受的未知类型参数可以用 @变量名接受
+        d.删除存储过程
+            DROP PROCEDURE [IF EXISTS] proc_name;
---
+++ MySQL/11.事务/事务.sql
@@ -0,0 +1,67 @@
+事务
+    1.事务由一个或多个sql语句组成的一个不可分割的整体，sql语句之间相互依赖，要么全部执行，要么全部都不执行
+
+    2.事务必须满足的四个条件
+        a.Atomicity 原子性
+        b.Consistency 一致性
+        c.Isolation 隔离性
+        d.Durability 持久性
+    3.控制事务处理
+        a.roollback 回滚会结束用户的事务，并撤销正在进行的所有未提交的修改
+        b.commit 会提交事务，并使已对数据库进行的所有修改成为永久性的
+        c.savepoint identifier 允许在事务中创建一个保存点，一个事物中可以有多个savepoint
+        d.roollback to identifier 把事务回滚到标记点
+
+    4.mysql事务处理主要有两种方法
+        a.用begin rollback commit 来实现
+            （1）begin 或 start transaction 开始一个事务
+            （2）rollback 事务回滚
+            （3）commit 事务确认
+        b.直接用set来改变mysql的自动提交模式
+            （1）set autocommit=0 禁止自动提交
+            （2）set autocommit=1 开启自动提交
+
+    5.示例
+        a.
+        create table mytest(
+            id int primary key,
+            name varchar(20)
+        );
+        use book;
+
+        /* 开始事务 */
+        begin;
+        insert into mytest values(1,'test1');
+        insert into mytest values(2,'test2');
+        commit;
+
+        select * from mytest;
+
+        /* 回滚事务 */
+        begin;
+        insert into mytest values(3,'test3');
+        rollback;
+
+        /* 设置保存点 */
+        begin;
+        insert into mytest values(4,'test4');
+        savepoint s1;
+        insert into mytest values(5,'test5');
+        savepoint s2;
+        insert into mytest values(6,'test6');
+        rollback to s1;
+        
+        b.
+        set autocommit=0;
+        insert into mytest values(7,'test7');
+        rollback;
+
+        set autocommit=1;
+        insert into mytest values(8,'test8');
+        rollback;
+
+    6.只有提交了事务，其他用户才能可见
+    
+通过InnoDB使用事务
+    1.InnoDB是事务型数据库的首选引擎，支持事务安全表（ACID）
+    2.mysql5.7以上InnoDB作为默认存储引擎，5.5.5前默MyISAM是默认存储引擎，当表需要事务处理时需要设置存储引擎为InnoDB
