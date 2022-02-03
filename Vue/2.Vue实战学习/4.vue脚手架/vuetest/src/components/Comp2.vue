44d42db1a98331421cb062d03e6efce8c5ea739c
HPME <443808626@qq.com>
2022-02-03 23:30:29 +0800

    2.3

---
+++ Vue/2.Vue实战学习/4.vue脚手架/vuetest/src/components/Comp2.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg:'你好'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
