e238bc4d64838920aa983da08a052d32f57fc303
HPME <443808626@qq.com>
2022-02-07 13:29:52 +0800

    2.7

--- Vue/2.Vue实战学习/4.vue脚手架/vuetest/vue.config.js
+++ Vue/2.Vue实战学习/4.vue脚手架/vuetest/vue.config.js
@@ -5,17 +5,17 @@ module.exports = {
     //     proxy:'http://localhost:8000'
     // }
     // 2.方式二
-    devServer:{
-        proxy:{
-            '/api':{//请求前缀
-                target:'http://localhost:8000',
-                pathRewrite:{'^/api':''},//重命名路径
-                ws:true,//用于支持websocket，默认为真
-                changeOrigin:true//用于控制请求头中的post值，默认为真
-            },
+    // devServer:{
+    //     proxy:{
+    //         '/api':{//请求前缀
+    //             target:'http://localhost:8000',
+    //             pathRewrite:{'^/api':''},//重命名路径
+    //             ws:true,//用于支持websocket，默认为真
+    //             changeOrigin:true//用于控制请求头中的post值，默认为真
+    //         },
             // '/foo':{
             //     target:'http://localhost:8000'
             // }
-        }
-    }
+    //     }
+    // }
   }
